00a4434b4ea875fc7cee26d342fd642a870847d5
Vedvardhan <vedvardhan.g@gmail.com>
2019-02-23 19:40:46 +0530

    test#

---
+++ script.js
@@ -0,0 +1,19 @@
+function volume() {
+    var r = parseFloat(document.getElementById('radius').value);
+
+    var output = document.getElementById('output');
+
+    var volume = 4/3*3.14*r*r*r;
+    output.textContent="Volume = "+volume;
+
+};
+
+function area() {
+    var radius = parseFloat(document.getElementById('radius1').value);
+
+    var ans= document.getElementById('ans');
+
+    var sa = 4*3.14*radius*radius;
+    ans.textContent= "Surface area = "+sa;
+
+};
